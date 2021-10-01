a8a2d87b8a8ef41f2799fb09a8a18187fa3df1e4
mr.b <bruno932151@gmail.com>
2021-09-30 23:56:16 -0500

    4-puts.c  was created

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+include < stdio.h >
+/*
+ * 4-puts.c - Print out the message "Programming is like building a multilingual puzzle"
+ */
+int main(void)
+{
+	puts(const char "\"Programming is like building a multilingual puzzle");
+	return(0);
+}
