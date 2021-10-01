408765dd40bb19c94771766c5cd601be80350731
mr.b <bruno932151@gmail.com>
2021-10-01 10:22:21 -0500

    4-puts.c was changed in line 1 and line 8

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
-include< stdio.h >
+include < stdio.h >
 /*
- * 4-puts.c - Print out the message "Programming is like building a multilingual puzzle"
+ * Print out the message -Programming is like building a multilingual puzzle
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+
+	return (0);
 }
