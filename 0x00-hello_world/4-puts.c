567520289cc16ff75e00206506e3123662e82e05
mr.b <bruno932151@gmail.com>
2021-09-30 23:58:35 -0500

    4-puts.c  was changed in line 1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,6 +4,6 @@ include< stdio.h >
  */
 int main(void)
 {
-	puts(const char "\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
