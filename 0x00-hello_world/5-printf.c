a8f12e090f7662a28dd076f853d886cf5937b415
mr.b <bruno932151@gmail.com>
2021-10-01 12:44:41 -0500

    : was added in 5-printf.c on line 4

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- * main - entry point
+ * main - use the function printf
  * 
- * Return 0 number
+ * Return: 0 number
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
+
