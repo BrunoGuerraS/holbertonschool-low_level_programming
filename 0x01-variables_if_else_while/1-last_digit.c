2a23f51bd1a441e38bfcaf0af1a8acad38eedc46
mr.b <bruno932151@gmail.com>
2021-10-02 22:50:26 -0500

    1-last_digit.c was modified

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,7 +15,7 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	m = n % 10;
-	if (m < 5)
+	if (1 <= m < 5)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
 	}
