40161ad84feb56b9e94370f5dfe3d4d34e31006a
mr.b <bruno932151@gmail.com>
2021-10-04 22:10:25 -0500

    5-print_numbers.c was created

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - print numbers 0 to 9 in promt
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int b = 48;
+
+	while (b <= 57)
+	{
+		putchar(b);
+		b++;
+	}
+
+	putchar(10);
+
+	return (0);
+}
