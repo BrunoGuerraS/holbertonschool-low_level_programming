d60e7170a72916a371040489165b5609da8a0b79
mr.b <bruno932151@gmail.com>
2021-10-06 23:37:10 -0500

    5-sign.c was created

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include <main.h>
+/**
+ * print_sign - print the sing
+ * @n: sing
+ * Return: c
+ **/
+int print_sign(int n)
+{
+	if (n >= 1)
+	{
+		_putchar(43);
+		n = 1;
+	}
+	else if (n <= -1)
+	{
+		_putchar(45);
+		n = -1;
+	}
+	else
+	{
+		_putchar(48);
+		n = 0;
+	}
+
+	return (n);
+
+}
