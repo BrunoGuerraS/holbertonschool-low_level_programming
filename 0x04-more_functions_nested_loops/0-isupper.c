2072a31002cd4d2a326fef831f3e48fc2049685b
mr.b <bruno932151@gmail.com>
2021-10-08 19:21:14 -0500

    0-isupper.c was creted

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * int _isupper - return if c is a letter in uppercase
+ * @c: int
+ * Return: c
+ */
+int int _isupper(int c)
+{
+	if (c > 64 && c < 91)
+	{
+		c = 1;
+	}
+	else
+	{
+		c = 0;
+	}
+
+	return (c);
+}
