bfa1abc9081aba8cba6ee3273ffe10eb8637550c
mr.b <bruno932151@gmail.com>
2021-10-08 22:03:34 -0500

    3-print_numbers.c was creted

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_numbers - unction that prints the numbers, from 0 to 9
+ *
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		_putchar(i);
+	}
+	_putchar(10);
+
+	return (0);
+}
