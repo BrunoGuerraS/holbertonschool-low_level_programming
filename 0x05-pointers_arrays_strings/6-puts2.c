2ab2cd670307846c0e9744e9fcc5c82f416d90fb
mr.b <bruno932151@gmail.com>
2021-10-12 22:48:07 -0500

    6-puts2.c was depurada

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,20 +1,19 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * puts2 - prints every other character of a string
  * @str: pointer
  */
 void puts2(char *str)
 {
-        while (*str != '\0')
-        {
-                _putchar(*str);
-                str++;
-		if(*str == '\0')
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+		if (*str == '\0')
 		{
 			break;
 		}
 		str++;
-		
-        }
-        _putchar(10);
+	}
+	_putchar(10);
 }
