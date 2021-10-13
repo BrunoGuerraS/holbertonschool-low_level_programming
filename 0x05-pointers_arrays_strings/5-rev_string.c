a3075043bb3a1ea8806215edbd203ff4b8020bac
mr.b <bruno932151@gmail.com>
2021-10-12 22:10:36 -0500

    betty checks

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -5,13 +5,12 @@
  */
 void rev_string(char *s)
 {
-	
+
 	int count = 0;
 	int i = 0;
 	char *ptr1 = s;
 	char *ptr2 = s;
 	char tmp;
-	
 
 	while (*ptr1 != '\0')
 	{
@@ -20,8 +19,8 @@ void rev_string(char *s)
 	}
 
 	ptr1--;
-	
-	while (i < count/2)
+
+	while (i < count / 2)
 	{
 		tmp = *ptr2;
 		*ptr2 = *ptr1;
@@ -30,5 +29,4 @@ void rev_string(char *s)
 		ptr1--;
 		i++;
 	}
-	_putchar(10);
 }
