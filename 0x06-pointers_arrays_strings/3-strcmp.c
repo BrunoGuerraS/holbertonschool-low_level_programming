4cebf2c6773c4fb2d98ee7973463b9ab357c77d3
mr.b <bruno932151@gmail.com>
2021-10-15 12:41:43 -0500

    3-strcmp.c was changed

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,7 +5,7 @@
  * @s2: pointer
  * Return: dest
  */
-int _strcmp(char *s1, char s2)
+int _strcmp(char *s1, char *s2)
 {
 	int x = 0;
 	int s1_acsii, s2_acsii;
@@ -21,6 +21,10 @@ int _strcmp(char *s1, char s2)
 			break;
 		}
 		x++;
+	}
+	if ((s1[x] == '\0') && (s2[x] == '\0'))
+	{
+		return (dif);
 	}
 
 	if ((s1[x] == '\0') || (s2[x] == '\0'))
