4c50da153e28feb95151a9cbb426c8d926d2e435
mr.b <bruno932151@gmail.com>
2021-10-15 12:33:45 -0500

    3-strcmp.c was created

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,10 @@
+#inlcude "main.h"
+/**
+ * *_strncpy - function that copies a string
+ * @dest: pointer
+ * @src: pointer
+ * @n: int
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
