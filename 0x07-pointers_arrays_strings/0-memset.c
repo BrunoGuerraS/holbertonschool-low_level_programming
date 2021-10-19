f5fc9853a884aafa1300a505df6f03f18f3eceea
mr.b <bruno932151@gmail.com>
2021-10-18 19:08:47 -0500

    0-memset.c the ponter was rescued

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,12 +9,13 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i = 0;
+	char *ptr = s
 
 	while (i < n)
 	{
-		*s = b;
+		*ptr = b;
 		i++;
-		s++;
+		ptr++;
 	}
 	return (s);
 }
