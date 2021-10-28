482b9561d81f59f0017328366c8de646651f661d
mr.b <bruno932151@gmail.com>
2021-10-27 22:28:19 -0500

    task 1 was passed test

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -6,8 +6,38 @@
  */
 char *_strdup(char *str)
 {
+	int acum = 0;
+	int x = 0;
+	char *ma;
+
 	if (str == NULL)
 	{
 		return (NULL);
 	}
+
+	while (str[x] != '\0')
+	{
+		acum += 1;
+		x++;
+	}
+
+	ma = malloc(sizeof(char) * (acum + 1));
+
+	if (!ma)
+	{
+		return (NULL);
+	}
+
+	x = 0;
+
+	while (str[x] != '\0')
+	{
+		ma[x] = str[x];
+		x++;
+	}
+
+	ma[x] = '\0';
+
+	return (ma);
+
 }
