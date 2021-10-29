57663ad8f89d37b6326e3cd3a59904212a9d58d3
mr.b <bruno932151@gmail.com>
2021-10-29 12:26:38 -0500

    readme main and 1 task was created

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * malloc_checked - hat allocates memory using malloc
+ * @b: unsigned int
+ * Return: 0
+ */
+void *malloc_checked(unsigned int b)
+{
+	char *ptr;
+
+	ptr = malloc(b);
+	if (a == NULL)
+	{
+		return (NULL);
+	}
+	return (ptr);
+	exit(98);
+}
