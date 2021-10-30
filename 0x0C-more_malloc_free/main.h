8438da48f6a90283c7a50da6633caefff20115fd
mr.b <bruno932151@gmail.com>
2021-10-29 22:54:30 -0500

    task 1 was created

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -5,8 +5,12 @@
 #include <limits.h>
 
 void *malloc_checked(unsigned int b);
+
 char *string_nconcat(char *s1, char *s2, unsigned int n);
+int _strlen(char *s)
+
 void *_calloc(unsigned int nmemb, unsigned int size);
+
 int *array_range(int min, int max);
 
 
