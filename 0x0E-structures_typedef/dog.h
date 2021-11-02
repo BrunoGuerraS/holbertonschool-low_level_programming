c1ba1ccd6bcbe1177ad137b992ae7c99e0627ff2
mr.b <bruno932151@gmail.com>
2021-11-02 18:21:15 -0500

    task 1 was created

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog's data
+ * @name: char
+ * @age: float
+ * @owner: char
+ * Description: a dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
