4b2d5d7fcff5e8fd1e7db3edf0c7dd7df74c5d1a
mr.b <bruno932151@gmail.com>
2021-11-02 19:31:47 -0500

    dog.h was changed

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,7 @@
 #ifndef DOG_H
 #define DOG_H
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct dog - dog's data
@@ -17,5 +19,6 @@ struct dog
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 #endif
