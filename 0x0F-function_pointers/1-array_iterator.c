017301ff8cb352d09383aa32de50257b057e0b7b
mr.b <bruno932151@gmail.com>
2021-11-03 21:26:23 -0500

    1-array_iterator.c was created

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - execute a function
+ * @array: int
+ * @size: int
+ * @action: pointer to function
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if(array == NULL || action == NULL)
+	{
+		return;
+	}
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
