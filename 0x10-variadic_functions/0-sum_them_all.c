27eb8c7ff51daa2642b94f1e69e8668e91191ff3
mr.b <bruno932151@gmail.com>
2021-11-04 19:22:09 -0500

    readme and header was created

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,9 @@
+#include "variadic_functions.h"
+/**
+ * sum_them_all - sum of all parameters
+ * @n: unsigned int
+ * Return: int
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+}
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,16 @@
+#ifndef VARIADIC_FUNCTION_H
+#define VARIADIC_FUNCTION_H
+#include <strdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+int sum_them_all(const unsigned int n, ...);
+
+void print_numbers(const char *separator, const unsigned int n, ...);
+
+void print_strings(const char *separator, const unsigned int n, ...);
+
+void print_all(const char * const format, ...);
+
+#endif
+
