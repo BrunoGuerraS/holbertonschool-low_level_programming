f954a87903518d4fe30f49dfe06b43f22bbdf32d
mr.b <bruno932151@gmail.com>
2021-11-04 20:48:40 -0500

    1-print_numbers.c was up date

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,6 +12,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 
 	va_start(ap, n);
+	if (separator == NULL)
+	{
+		return;
+	}
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(ap, int));
