81171960bad04723fd8536a3bd9c428e60a75c4c
mr.b <bruno932151@gmail.com>
2021-11-04 20:05:34 -0500

    0-sum_them_all.c was created

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -6,4 +6,14 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
+	va_list ap;
+	unsigned int i;
+	int sum = 0;
+
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(ap, int);
+	}
+	return (sum);
 }
