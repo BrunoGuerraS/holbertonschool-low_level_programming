57d13d9f271e43db1e08b0db1c3a9eb5d9a28c68
mr.b <bruno932151@gmail.com>
2021-11-05 22:59:52 -0500

    3-print_all.c was created

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,50 @@
+#include "variadic_functions.h"
+/**
+ * print_all - unction that prints anything
+ * @format: type of data
+ */
+void print_all(const char * const format, ...);
+{
+	int i = 0;
+	char *s;
+	char *separator = "";
+
+	va_list ap;
+
+	va_start(ap, format);
+
+	if (format)
+	{
+		while (format[i])
+		{
+			swich(format[i])
+			{
+			case 'c':
+				printf("%s%c", separator, va_arg(ap, int));
+				break;
+			case 'i':
+				printf("%s%c", separator, va_arg(ap, int));
+				break;
+			case 'f':
+				printf("%s%c", separator, va_arg(ap, double));
+				break;
+			case 's':
+				s = va_arg(ap, char *);
+				if (!s)
+				{
+					s = "(nil)";
+				}
+				printf("%s%c", separator, s);
+				break;
+			default:
+				i++;
+				continue;
+			}
+			separator = ", ";
+			i++
+
+		}
+	}
+	putchar(10);
+	va_end(ap);
+}
