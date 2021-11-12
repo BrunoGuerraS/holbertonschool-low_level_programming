0b542da08098d71708eb48a90dc60482c79163f0
mr.b <bruno932151@gmail.com>
2021-11-11 20:16:32 -0500

    0-print_list.c was update

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,17 +7,16 @@
 size_t print_list(const list_t *h)
 {
 	size_t count = 0;
-	const list_t *whisky = h;
 
-	while (whisky)
+	while (h)
 	{
-		if (whisky->str == NULL)
+		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
 		{
-			printf("[%u] %s\n", whisky->len, whisky->str);
+			printf("[%u] %s\n", h->len, h->str);
 		}
-		whisky = whisky->next;
+		h = h->next;
 		count++;
 	}
 	return (count);
