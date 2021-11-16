75d0b356d80d6d0d7b67b50a01b0e2aadb358da9
mr.b <bruno932151@gmail.com>
2021-11-16 17:01:56 -0500

    task 4 was up date

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,3 +1,13 @@
 #include "lists.h"
 /**
- *
+ * free_listint - frees
+ * @head: pointer to struct
+ */
+void free_listint(listint_t *head)
+{
+	if (!head)
+		return;
+	free_listint(head->next);
+	free(head->n);
+	free(head);
+}
