ffa3b1e3d1e9731d1428b1a84c8d464c1636608e
mr.b <bruno932151@gmail.com>
2021-11-16 16:36:14 -0500

    task 3 was up date

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -11,6 +11,10 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *tmp = *head;
 
 	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
 	new->n = n;
 	new->next = NULL;
 	if (*head == NULL)
