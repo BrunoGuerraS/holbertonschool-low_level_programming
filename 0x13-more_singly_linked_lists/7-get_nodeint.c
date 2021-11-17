b88d5eb829cbedd2b0714b034b5ccd62eb6d4fc1
mr.b <bruno932151@gmail.com>
2021-11-17 16:27:33 -0500

    task 7 was created

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_index - print nts node
+ * @head: pointer to header
+ * @index: size_t
+ * Return: pointer to struct
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	size_t count = 0;
+	listint_t *tmp = head;
+
+	if (head == NULL || index < 0)
+		return (NULL);
+	if (index == 0)
+		return (head);
+	while (count < index)
+	{
+		tmp = tmp->next;
+		count++;
+	}
+	return (tmp);
+}
