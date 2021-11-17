5399149d752c49c00753648b481443afb05651cc
mr.b <bruno932151@gmail.com>
2021-11-17 17:05:07 -0500

    7-get_nodeint.c was up date

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,7 +10,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	unsigned int count = 0;
 	listint_t *tmp = head;
 
-	if (head == NULL || index < 0)
+	if (head == NULL)
 		return (NULL);
 	if (index == 0)
 		return (head);
