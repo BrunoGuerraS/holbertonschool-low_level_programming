9299645366a70d0d82da0b7e9aaf010b97af76bf
mr.b <bruno932151@gmail.com>
2021-11-17 23:19:19 -0500

    task 14 was created

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+/**
+ * find_listint_loop - finds the loop in a linked list
+ * @head: linked list to search for
+ * Return: address of the node where the loop starts, or NULL
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *sl = head;
+	listint_t *ft = head;
+
+	if (!head)
+		return (NULL);
+	while (sl && ft && ft->next)
+	{
+		ft = ft->next->next;
+		sl = sl->next;
+		if (ft == sl)
+		{
+			sl = head;
+			while (sl != ft)
+			{
+				sl = sl->next;
+				ft = ft->next;
+			}
+			return (ft);
+		}
+	}
+	return (NULL);
+}
