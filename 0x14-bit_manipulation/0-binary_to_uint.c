5695c65a79f2bfef1639f38ed66b95463b838b0f
mr.b <bruno932151@gmail.com>
2021-11-19 10:48:12 -0500

    task 0 was created

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,5 +6,22 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	
+	/*un unsi int bacio que sera llenado bit por bit*/
+	unsigned int a = 0;
+
+	/*desde el comienzo el puntero puede estar vacio*/
+	if (b == 0)
+		return (0);
+
+	/*recorreremos el array*/
+	while (*b)
+	{
+		if (*b != '0' && *b != '1')
+			return(0);
+		a = a << 1
+		if (*b == '1')
+			a = a ^ 1;
+		b++;
+	}
+	return (a);
 }
