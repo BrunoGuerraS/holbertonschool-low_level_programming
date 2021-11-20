964fa77aca87dae3da0bb3821a8745438adf1794
mr.b <bruno932151@gmail.com>
2021-11-19 21:54:34 -0500

    task 3 was created

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * set_bit - search in the index
+ * @n: unsigned long int
+ * @index: the index
+ * Return: int
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > sizeof(unsigned long int) * 8)
+		return (-1);
+	*n = ((usigned long int << index) | *n
+	return (1);)
+}
