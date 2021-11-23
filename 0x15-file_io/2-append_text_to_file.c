51ca414e222de14a09b9959a7a70687b2d057775
mr.b <bruno932151@gmail.com>
2021-11-22 22:50:29 -0500

    task2 was up date

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -13,9 +13,9 @@ int append_text_to_file(const char *filename, char *text_content)
 	int i = 0;
 
 	if(filename == NULL)
-		return (-1)
+		return (-1);
 
-	whisky = open(filename, O_CREAT | O_WRONLY | O_APPEND, 0600)
+	whisky = open(filename, O_CREAT | O_WRONLY | O_APPEND, 0600);
 	if (whisky == -1)
 		return (-1);
 
