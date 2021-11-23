2efbc3708444ee2d8773ecfeee875ff7de2ff221
mr.b <bruno932151@gmail.com>
2021-11-22 22:45:03 -0500

    task2 was created

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * append_text_to_file - function that appends text at the end of a file
+ * @filename: name of the file
+ * @text_content: the text
+ * Return: int
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int whisky;
+	int vodka;
+	char *s = text_content;
+	int i = 0;
+
+	if(filename == NULL)
+		return (-1)
+
+	whisky = open(filename, O_CREAT | O_WRONLY | O_APPEND, 0600)
+	if (whisky == -1)
+		return (-1);
+
+	if(text_content != NULL)
+	{
+		while (*s != '\0')
+		{
+			i++;
+			s++;
+		}
+		vodka = write(whisky, text_content, i);
+		if (vodka == -1)
+			return (-1);
+	}
+	return (1);
+}
