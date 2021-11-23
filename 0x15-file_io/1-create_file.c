92ad4edaa89e0d63afefa7c293f24dfe86ae6620
mr.b <bruno932151@gmail.com>
2021-11-22 22:12:41 -0500

    task1 was up date

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	whisky = open(filename, O_CREAT | O_WRONLY | O_TRUNC , 0600);
+	whisky = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (whisky == -1)
 		return (-1);
 	if (text_content != NULL)
