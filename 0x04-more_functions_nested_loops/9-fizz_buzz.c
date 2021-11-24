042d9c89baee4c944948c60c6b9a857f2430b026
mr.b <bruno932151@gmail.com>
2021-11-24 15:16:36 -0500

    task 9 was created

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * main - prints from 0 to 100
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	n = 1;
+	while (n <= 100)
+	{
+		if ((n % 3 == 0 || n % 5 == 0) || (n % 3 == 0 && n % 5 == 0))
+		{
+			if (n % 3 == 0 && n % 5 == 0)
+				printf("FizzBuzz");
+			else if (n % 3 == 0)
+				printf("Fizz");
+			else if (n % 5 == 0)
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", n);
+		}
+		if (n < 100)
+		{
+			putchar(32);
+		}
+		n++;
+	}
+	putchar(10);
+	return (0);
+}
