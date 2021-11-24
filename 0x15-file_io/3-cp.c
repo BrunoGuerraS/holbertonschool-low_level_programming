1436e23c3458df30a5cabda9fbe05daedffe9345
mr.b <bruno932151@gmail.com>
2021-11-23 21:23:13 -0500

    task3 was up date

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,37 @@
 #include "main.h"
 /**
+ * _fill - handle arguments to copy text
+ * @argv: arguments
+ * @fd_from: file from
+ * @fd_to: file to
+ */
+void _fill(char **argv, int fd_from, int fd_to)
+{
+	int MAX_BUFF = 1024;
+	char buffer[1024];
+	int rd = 1;
+	int wr;
+
+	while (1)
+	{
+		if (rd == 0)
+			break;
+		rd = read(fd_from, buffer, MAX_BUFF);
+		if (rd == -1)
+		{
+			dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
+
+		wr = write(fd_to, buffer, rd);
+		if (rd == -1)
+		{
+			dprintf(2, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	}
+}
+/**
  * main - copi a file
  * @argc: number of elements
  * @argv: array of elements
@@ -22,8 +54,6 @@ int main(int argc, char **argv)
 	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC,  0664);
 	if (fd_to == -1)
 		exit(97);
-
 	_fill(argv, fd_from, fd_to);
-
 	return (0);
 }
