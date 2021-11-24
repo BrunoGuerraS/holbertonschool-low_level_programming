b0488e057551ef33b6af04895610ea6468b4393f
mr.b <bruno932151@gmail.com>
2021-11-24 13:05:36 -0500

    task 8 was created

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_square - print a square
+ * @size: size of square
+ */
+void print_square(int size)
+{
+        int i, o;
+        i = size;
+        o = size;
+
+        if (size > 0)
+        {
+                while (i > 0)
+                {
+                        o = size;
+                        while (o > 0)
+                        {
+                                putchar(35);
+                                o--;
+                        }
+                        putchar(10);
+                        i--;
+                }
+        }
+        else
+                putchar(10);
+}
