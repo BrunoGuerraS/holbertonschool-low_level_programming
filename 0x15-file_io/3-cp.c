ab17c42fd83c164b30f8f69be9250456800b0847
mr.b <bruno932151@gmail.com>
2021-11-23 22:08:10 -0500

    task 3 was up date

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -5,55 +5,46 @@
  * @fd_from: file from
  * @fd_to: file to
  */
-void _fill(char **argv, int fd_from, int fd_to)
+int main(int argc, char *argv[])
 {
-	int MAX_BUFF = 1024;
-	char buffer[1024];
-	int rd = 1;
-	int wr;
-
-	while (1)
-	{
-		if (rd == 0)
-			break;
-		rd = read(fd_from, buffer, MAX_BUFF);
-		if (rd == -1)
-		{
-			dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-			exit(98);
-		}
-
-		wr = write(fd_to, buffer, rd);
-		if (rd == -1)
-		{
-			dprintf(2, "Error: Can't write to %s\n", argv[2]);
-			exit(99);
-		}
-	}
-}
-/**
- * main - copi a file
- * @argc: number of elements
- * @argv: array of elements
- * Return: 0
- */
-int main(int argc, char **argv)
-{
-	int fd_from;
-	int fd_to;
+	int fd_r, fd_w, r, a, b;
+	char buf[BUFSIZ];
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
-		exit(97);
-
-	fd_to = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY,  0666);
-	if (fd_to == -1)
-		exit(97);
-	_fill(argv, fd_from, fd_to);
+	fd_r = open(argv[1], O_RDONLY);
+	if (fd_r < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	fd_w = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	while ((r = read(fd_r, buf, BUFSIZ)) > 0)
+	{
+		if (fd_w < 0 || write(fd_w, buf, r) != r)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			close(fd_r);
+			exit(99);
+		}
+	}
+	if (r < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	a = close(fd_r);
+	b = close(fd_w);
+	if (a < 0 || b < 0)
+	{
+		if (a < 0)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_r);
+		if (b < 0)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_w);
+		exit(100);
+	}
 	return (0);
 }
