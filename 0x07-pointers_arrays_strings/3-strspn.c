d6d433b28c086d4671e485da26e05c45c6ac0f3f
mr.b <bruno932151@gmail.com>
2021-12-09 08:54:38 -0500

    task 3 was created

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _strspn - function that gets the length of a prefix substring.
+ * @s: pointer
+ * @accept: pointer
+ * Return: s
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	
+}
