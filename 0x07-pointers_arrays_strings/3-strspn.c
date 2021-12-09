b5ad406b225cad199182ba46ab917b7807f56911
mr.b <bruno932151@gmail.com>
2021-12-09 09:32:17 -0500

    task3 was up date

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,5 +7,26 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	
+	int x = 0;
+	int y = 0;
+	int k = 0;
+	int z = 0;
+
+	while (*(s + x) ¡=)
+	{
+		k = 0;
+		while (*(accept + y) != 0)
+		{
+			if (*(s + x) == *(accept + y))
+			{
+				z++;
+				k = 1;
+			}
+			y++;
+		}
+		if (k == 0)
+			return (z);
+		x++;
+	}
+	return (0);
 }
