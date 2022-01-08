53c469ef9b1e30a1a9260576579365e37edc97fd
mr.b <bruno932151@gmail.com>
2022-01-07 22:11:25 -0500

    update

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,39 +1,37 @@
 #include "lists.h"
-
 /**
- * add_dnodoint_end - Adds a new nodo at the end of a dlistint_t list.
+ * add_dnodeint_end - Adds a new node at the end of a dlistint_t list.
  * @head: Double pointer to struct.
- * @n: data to insert in the new nodo.
+ * @n: data to insert in the new node.
  *
- * Return: The address of the new element, or NULL if it failed.
+ * Return: The address of the new element
  */
-dlistint_t *add_dnodoint_end(dlistint_t **head, const int n)
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *nodo = NULL, *tmp = *head;
+	dlistint_t *node = NULL, *tmp = *head;
 
 	if (head == NULL)
 		return (NULL);
 
-	nodo = malloc(sizeof(dlistint_t));
-	if (!nodo)
+	node = malloc(sizeof(dlistint_t));
+	if (!node)
 	{
 		dprintf(2, "Error: Can't malloc\n");
 		return (NULL);
 	}
-	nodo->n = n;
-	nodo->next = NULL;
+	node->n = n;
+	node->next = NULL;
 	if (*head == NULL)
 	{
-		nodo->prev = NULL;
-		(*head) = nodo;
-		return (nodo);
+		node->prev = NULL;
+		(*head) = node;
+		return (node);
 	}
-
 	while (tmp->next)
 	{
 		tmp = tmp->next;
 	}
-	tmp->next = nodo;
-	nodo->prev = tmp;
-	return (nodo);
+	tmp->next = node;
+	node->prev = tmp;
+	return (node);
 }
