0fe721bc5354cec831320ea8d2550d2447a44718
mr.b <bruno932151@gmail.com>
2022-01-28 16:44:44 -0500

    task0 was update

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,13 +8,15 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht;
 
+	if (size <= 0)
+		return (NULL);
 	ht = malloc(sizeof(hash_table_t) * 1);
 	if (ht == NULL)
 		return (NULL);
 
 	ht->size = size;
 	/*calloc clean memory*/
-	ht->array = calloc(size, sizeof(hash_node_t));
+	ht->array = calloc(size, sizeof(hash_node_t *));
 	if (ht->array == NULL)
 	{
 		free(ht);
