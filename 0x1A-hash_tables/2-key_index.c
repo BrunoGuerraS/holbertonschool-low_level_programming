5f88f5e356a5b5aeb876a2463ff5498b3ac8d96c
mr.b <bruno932151@gmail.com>
2022-01-28 21:14:08 -0500

    task2 was created

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
+#include "hash_tables.h"
 /**
  * key_index - a function that gives you the index of a key
  * @key: key
  * @size: size
- * Return: index 
+ * Return: index
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
