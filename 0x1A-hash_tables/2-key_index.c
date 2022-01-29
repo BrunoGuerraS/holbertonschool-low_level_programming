9bc4cab040563e3678984081c44e500fb41d11ab
mr.b <bruno932151@gmail.com>
2022-01-28 20:56:16 -0500

    task2 was created

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * key_index - a function that gives you the index of a key
+ * @key: key
+ * @size: size
+ * Return: index 
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
