ca7a2a1e517fb343dcf3f67d53c6f8d9288a4766
mr.b <bruno932151@gmail.com>
2022-01-29 16:11:41 -0500

    †ask 4 was created

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,24 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get -retrieves a value associated with a key
+ * @ht: hash_tablet
+ * @key: a key value
+ * Return: value
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned int index = 0;
+	hash_node_t *bringh = NULL;
+
+	if (ht == NULL || key == NULL || !strcmp(key, ""))
+		return (NULL);
+	index = key_index((const unsigned char *)key, ht->size);
+	bringh = ht->array[index];
+	while (bringh)
+	{
+		if (strcmp(bringh->key, (char *)key) == 0 )
+			return (bringh->value);
+		bringh = bringh->next;
+	}
+	return (NULL);
+}
