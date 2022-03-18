0b5a54b60ab331ddc4e9ef464ab93165b76584a0
mr.b <bruno932151@gmail.com>
2022-03-17 23:28:22 -0500

    task 5 was update

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" 5. Island Perimeter """
+
+
+def island_perimeter(grid):
+    """ Returns a grid"""
+    counter = 0
+    max_grid = len(grid) > 100 if len(grid) == 100 else len(grid)
+    for row in range(max_grid):
+        max_row = len(grid[row]) > 100 \
+            if len(grid[row]) == 100 else len(grid[row])
+        for col in range(max_row):
+            if grid[row][col] == 1:
+                top = grid[row - 1][col] if row - 1 != -1 else 0
+                bot = grid[row + 1][col] if row + 1 != max_grid else 0
+                left = grid[row][col - 1] if col - 1 != -1 else 0
+                right = grid[row][col + 1] if col + 1 != max_row else 0
+                if top == 0:
+                    count += 1
+                if bot == 0:
+                    count += 1
+                if left == 0:
+                    count += 1
+                if right == 0:
+                    count += 1
+    return counter
