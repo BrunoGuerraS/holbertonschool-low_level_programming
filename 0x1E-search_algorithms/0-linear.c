eefe96769eb244c5a279bfe685d83d348e09bc42
mr.b <bruno932151@gmail.com>
2022-05-03 18:48:51 -0500

    task0 was update

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,21 @@
+#include "search_algos.h"
+/**
+ * linear_search - searches for a value in an array
+ * @array: list of numbers
+ * @size: number of elements
+ * @value: value
+ * Return: 0 or  -1
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		if (value != array[i])
+			printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		else
+			break;
+	};
+	return ((int)i);
+}
